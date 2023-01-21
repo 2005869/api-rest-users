fae7658268614013e52b36cd757ffaba4c4946c5
2005869 <2005869@aluno.univesp.br>
2023-01-21 14:29:33 -0300

    change info in home

--- users/src/views/HomeView.vue
+++ users/src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="content is-normal">
+    <h1>Hello World</h1>
+    <p>This is a simple page to use with an API Rest.</p>
+    <h2>What can I do?</h2>
+    <p>You can</p>
+    <ul>
+      <li>Register how a user privilege</li>
+      <li>Do login</li>
+      <li>If you're a Admin user, you can view and edit the info about users</li>
+      <li>There is still a lot of work to do...</li>
+    </ul>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    //HelloWorld
   }
 }
 </script>
